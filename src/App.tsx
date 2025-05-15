a5d32576b9cf120e9d70878429a87340b2057a2f
Joker <puebloseko@gmail.com>
2025-05-15 18:03:53 +0200

    basic configuracion of routes and react-route

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,21 @@
+import {Routes,Route} from 'react-router-dom' 
 import "./App.css";
+import Home from "./pages/Home/Home.tsx";
+import Shop from './pages/Shop/Shop.tsx';
 import Header from "./components/Header/Header.tsx";
 import ServicesBox from "./components/ServicesBox/ServicesBox.tsx";
 
 function App() {
   return (
     <>
-      <Header />
+      <Routes>
+         <Route path="/" element= {<Home/>}/>
+         <Route path="/Shop" element={<Shop/>}/>
+      </Routes>
+       <Header />
       <ServicesBox />
+
+     
     </>
   );
 }
---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,14 @@
+import { Link } from "react-router-dom";
+
+function Home() {
+  return (
+    <div>
+        Home jejejej
+        <Link to="/Shop"> <button>COMPRA</button>  </Link>
+        
+
+    </div>
+  )
+}
+
+export default Home

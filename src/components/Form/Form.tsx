943c220ce23c7cf8a1ee3114fb4d6d12917530f5
Joker <puebloseko@gmail.com>
2025-05-23 15:54:47 +0200

    WIP show many budgets

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -1,7 +1,9 @@
-import { useContext } from "react";
+import { useContext,useState} from "react";
 import {
   ContextTotalItems,
   ContextTotalPrice,
+  ContextSetBudgetArray,
+  ContextBudgetArray
 } from '../../App';
 import {
   CardContainer,
@@ -13,18 +15,38 @@ import {
 function Form() {
   const total = useContext(ContextTotalPrice);
   const totalItems = useContext(ContextTotalItems);
+  const setBudgetArray = useContext(ContextSetBudgetArray);
+  const budgetArray = useContext(ContextBudgetArray);
+
+  const [name,setName]=useState<string>("");
+  const [email, setEmail] = useState<string>("");
+  const [tlf, setTlf] = useState<string>("");
 
   function addCardBudget() {
-    alert("Añadido a Mis Presupuestos " + (total + totalItems));
+    alert("Añadido a Mis Presupuestos " + (total + totalItems) + name + email + tlf);
   }
+
+  function handleOnChangeName(e){
+    setName(e.target.value);
+  }
+
+  function handleOnChangeEmail(e) {
+  setEmail(e.target.value);
+}
+
+function handleOnChangeTlf(e) {
+  setTlf(e.target.value);
+}
+
+
 
   return (
     <CardContainer>
       <h1>Demanar pressupost</h1>
       <FormContainer>
-        <Input type="text" placeholder="Nombre" />
-        <Input type="text" placeholder="Email" />
-        <Input type="text" placeholder="Teléfono" />
+        <Input type="text" placeholder="Nombre" value={name}  onChange={handleOnChangeName}  />
+        <Input type="text" placeholder="Email"value={email} onChange={handleOnChangeEmail}/>
+        <Input type="text" placeholder="Teléfono" value={tlf} onChange={handleOnChangeTlf} />
         <RedButton onClick={addCardBudget}>Go!</RedButton>
       </FormContainer>
     </CardContainer>
